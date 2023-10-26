d764f4c5ec86eb8c75b3b2950aadd5685a010e89
Kiran Kumar HK <kiranhk98@gmail.com>
2023-10-25 19:18:52 -0700

    added DFS logic

--- search.py
+++ search.py
@@ -87,7 +87,45 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    # Return type is a list of actions to be performed to reach the goal
+
+    # print "Start:", problem.getStartState()
+    # print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    # print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    
+    visited = set()
+    stack = util.Stack()
+    stack.push((problem.getStartState(), ""))
+    parentNode = {}
+    goalNode = None
+
+    while not stack.isEmpty():
+        top = stack.pop()
+        # print "Here:", top
+
+        if problem.isGoalState(top[0]):
+            # print(top[0],problem.isGoalState(top[0]))
+            goalNode = top
+            break
+        
+        visited.add(top[0])
+        for neighbor in problem.getSuccessors(top[0]):
+            if neighbor[0] not in visited:
+                stack.push((neighbor[0], neighbor[1]))
+                parentNode[neighbor[0]] = top
+
+    actions = []
+
+    # for x in parentNode:
+    #     print(x, parentNode[x])
+    while goalNode[0] is not problem.getStartState():
+        # print(goalNode,  problem.getStartState())
+        actions.append(goalNode[1])
+        goalNode = parentNode[goalNode[0]]
+
+    # print(actions)
+    return actions[::-1]
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
